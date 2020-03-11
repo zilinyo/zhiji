f00ae1fbb9100e545723c94b8b6e7a11ad0bf006
zilinyo <zilinyo@gmail.com>
2020-03-11 22:51:21 +0800

    修复gitignore

--- api/conf/params.go
+++
@@ -1,7 +0,0 @@
-package conf
-
-const (
-	DOMAIN   = "https://zhiji-1253509153.cos.ap-shanghai.myqcloud.com/"
-	SECREID  = "AKIDyl13pNrliYBZjofLl00lfMp31MVXmr9B"
-	SECREKEY = "vbNuJTNAhy0A5sQjSxDEAsSntWv5Cfui"
-)
